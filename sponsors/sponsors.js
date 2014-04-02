f780f96be7b2ff1448d28d67c8752962a7d2d4bb
windley <pjw@kynetx.com>
2014-04-02 09:40:29 -0600

    CNAME in place

--- sponsors/sponsors.js
+++ sponsors/sponsors.js
@@ -3,50 +3,42 @@ var sponsors = [
  {"name": "Neustar",
   "url": "http://www.neustar.com/",
   "logo": "sponsors/logos/neustar.png",
-  "what": "Barista",
-  "color": "tile-teal"
+  "what": "Barista"
  },
  {"name": "Microsoft",
   "url": "http://www.microsoft.com/",
   "logo": "sponsors/logos/MSFT.png",
-  "what": "Conference Dinner",
-  "color": "tile-blue"
+  "what": "Conference Dinner"
  },
  {"name": "Google",
   "url": "http://www.google.com/",
   "logo": "sponsors/logos/GOOG.jpg",
-  "what": "Lunch & Doc Center",
-  "color": "tile-pink"
+  "what": "Lunch & Doc Center"
  },
  {"name": "Gigya",
   "url": "http://www.gigya.com/",
   "logo": "sponsors/logos/gigya.jpg",
-  "what": "Morning Breaks",
-  "color": "tile-sun-flower"
+  "what": "Morning Breaks"
  },
  {"name": "Salesforce",
   "url": "http://www.salesforce.com",
   "logo": "sponsors/logos/salesforce.png",
-  "what": "Lunch",
-  "color": "tile-wisteria"
+  "what": "Lunch"
  },
  {"name": "ForgeRock",
   "url": "http://www.forgerock.com",
   "logo": "sponsors/logos/forgerock.png",
-  "what": "Reception",
-  "color": "tile-carrot"
+  "what": "Reception"
  },
  {"name": "Yubico",
   "url": "http://www.yubico.com/",
   "logo": "sponsors/logos/yubico.png",
-  "what": "Break",
-  "color": "tile-teal"
+  "what": "Break"
  },
-    {"name": "IoPT Consulting",
+ {"name": "IoPT Consulting",
   "url": "https://ioptconsulting.com/",
   "logo": "sponsors/logos/iopt.png",
-  "what": "Gifting",
-  "color": "tile-blue"
+  "what": "Gifting"
  }
 
 
@@ -64,12 +56,24 @@ var sponsors = [
 
 ];
 
+var colors = [
+    "tile-teal",
+    "tile-blue",
+    "tile-pink",
+    "tile-sun-flower",
+    "tile-wisteria",
+    "tile-carrot"
+];
+
 
 $( document ).ready(function() {
+
+    var source   = $("#sponsor-template").html();
+    var template = Handlebars.compile(source);
  
+    var count = 0;
     jQuery.each(sponsors, function(index,value) {
-	var source   = $("#sponsor-template").html();
-	var template = Handlebars.compile(source);
+	value["color"] = colors[count++ % 6];
 	$("#sponsors").append(template(value));
     });
 
