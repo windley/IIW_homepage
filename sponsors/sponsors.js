84632bc41882d990bcc910939c4d93188710d278
Phil Windley <phil@windley.org>
2017-01-04 11:15:38 -0700

    sponsors

--- sponsors/sponsors.js
+++ sponsors/sponsors.js
@@ -1,85 +1,85 @@
 
 var sponsors = [
- {"name": "Google",
-  "url": "http://www.google.com",
-  "logo": "sponsors/logos/googlelogo_light_clr_160x56pxx.png",
-  "what": "Welcome Dinner" 
- },
- {"name": "Microsoft",
-  "url": "http://www.microsoft.com",
-  "logo": "sponsors/logos/MSFT.png",
-  "what": "Conference Dinner"
- }, 
+ // {"name": "Google",
+ //  "url": "http://www.google.com",
+ //  "logo": "sponsors/logos/googlelogo_light_clr_160x56pxx.png",
+ //  "what": "Welcome Dinner" 
+ // },
+ // {"name": "Microsoft",
+ //  "url": "http://www.microsoft.com",
+ //  "logo": "sponsors/logos/MSFT.png",
+ //  "what": "Conference Dinner"
+ // }, 
 {"name": "VMWare",
   "url": "https://www.vmware.com/",
   "logo": "sponsors/logos/vmware.png",
   "what": "Barista"
- },
- {"name": "Gigya",
-  "url": "http://www.gigya.com",
-  "logo": "sponsors/logos/gigya.jpg",
-  "what": "BBQ Lunch"
- },
- {"name": "ForgeRock",
-  "url": "https://www.forgerock.com/",
-  "logo": "sponsors/logos/forgerock.png",
-  "what": "Tables & Power"
- },
- {"name": "Kantara",
-  "url": "https://kantarainitiative.org/",
-  "logo": "sponsors/logos/logo_kantara.gif",
-  "what": "Projectors"
- },
- {"name": "Oracle",
-  "url": "https://oracle.com/",
-  "logo": "sponsors/logos/oracle.png",
-  "what": "Lunch"
- },
- {"name": "digi.me",
-  "url": "https://digi.me",
-  "logo": "sponsors/logos/digi.me-logo.png",
-  "what": "Conference Reception"
- },
- {"name": "Login with Amazon",
-  "url": "http://login.amazon.com/",
-  "logo": "sponsors/logos/login_with_amazon.png",
-  "what": "Demo Hour"
- },
- {"name": "Janrain",
-  "url": "https://www.janrain.com/",
-  "logo": "sponsors/logos/janrain.png",
-  "what": "Opening Reception"
- },
- {"name": "Yubico",
-  "url": "https://www.yubico.com/", 
-  "logo": "sponsors/logos/yubico.png",
-  "what": "Tuesday Breakfast"
- },
- {"name": "Identity.com",
-  "url": "http://www.identity.com",
-  "logo": "sponsors/logos/iD_logo_vertical.png",
-  "what": "Afternoon Break"
- },
- {"name": "Evernym",
-  "url": "http://www.evernym.com",
-  "logo": "sponsors/logos/evernym.png",
-  "what": "Morning Break"
- },
- {"name": "BIG",
-  "url": "http://bigfintechmedia.com/",
-  "logo": "sponsors/logos/big.png",
-  "what": "Afternoon Break"
- },
- {"name": "Constellation Research",
-  "url": "https://www.constellationr.com",
-  "logo": "sponsors/logos/constellation-logo.png",
-  "what": "Doc Center"
- },
- {"name": "Evernym",
-  "url": "http://www.evernym.com",
-  "logo": "sponsors/logos/evernym.png",
-  "what": "Gifting"
  }
+ // {"name": "Gigya",
+ //  "url": "http://www.gigya.com",
+ //  "logo": "sponsors/logos/gigya.jpg",
+ //  "what": "BBQ Lunch"
+ // },
+ // {"name": "ForgeRock",
+ //  "url": "https://www.forgerock.com/",
+ //  "logo": "sponsors/logos/forgerock.png",
+ //  "what": "Tables & Power"
+ // },
+ // {"name": "Kantara",
+ //  "url": "https://kantarainitiative.org/",
+ //  "logo": "sponsors/logos/logo_kantara.gif",
+ //  "what": "Projectors"
+ // },
+ // {"name": "Oracle",
+ //  "url": "https://oracle.com/",
+ //  "logo": "sponsors/logos/oracle.png",
+ //  "what": "Lunch"
+ // },
+ // {"name": "digi.me",
+ //  "url": "https://digi.me",
+ //  "logo": "sponsors/logos/digi.me-logo.png",
+ //  "what": "Conference Reception"
+ // },
+ // {"name": "Login with Amazon",
+ //  "url": "http://login.amazon.com/",
+ //  "logo": "sponsors/logos/login_with_amazon.png",
+ //  "what": "Demo Hour"
+ // },
+ // {"name": "Janrain",
+ //  "url": "https://www.janrain.com/",
+ //  "logo": "sponsors/logos/janrain.png",
+ //  "what": "Opening Reception"
+ // },
+ // {"name": "Yubico",
+ //  "url": "https://www.yubico.com/", 
+ //  "logo": "sponsors/logos/yubico.png",
+ //  "what": "Tuesday Breakfast"
+ // },
+ // {"name": "Identity.com",
+ //  "url": "http://www.identity.com",
+ //  "logo": "sponsors/logos/iD_logo_vertical.png",
+ //  "what": "Afternoon Break"
+ // },
+ // {"name": "Evernym",
+ //  "url": "http://www.evernym.com",
+ //  "logo": "sponsors/logos/evernym.png",
+ //  "what": "Morning Break"
+ // },
+ // {"name": "BIG",
+ //  "url": "http://bigfintechmedia.com/",
+ //  "logo": "sponsors/logos/big.png",
+ //  "what": "Afternoon Break"
+ // },
+ // {"name": "Constellation Research",
+ //  "url": "https://www.constellationr.com",
+ //  "logo": "sponsors/logos/constellation-logo.png",
+ //  "what": "Doc Center"
+ // },
+ // {"name": "Evernym",
+ //  "url": "http://www.evernym.com",
+ //  "logo": "sponsors/logos/evernym.png",
+ //  "what": "Gifting"
+ // }
  // {"name": "AOL",
  //  "url": "https://www.aol.com/", 
  //  "logo": "sponsors/logos/Aol.white.png",
@@ -133,11 +133,6 @@ var sponsors = [
 ];
 
 var sponsors_old = [
- {"name": "Neustar",
-  "url": "http://www.neustar.com",
-  "logo": "sponsors/logos/neustar.png",
-  "what": "Barista"
- },
  {"name": "Nexus Group",
   "url": "https://www.nexusgroup.com/",
   "logo": "sponsors/logos/nexus.jpeg",
